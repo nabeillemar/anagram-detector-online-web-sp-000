b5adbf12188758790ad3883296c79de0b725b353
Nabeil Lemar <nabeillemar@gmail.com>
2020-11-14 20:23:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :name
+@@all = []
+
+def self.all
+  @@all
+end
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def match()#takes an array of possible anagrams
+end
+
+
+end
