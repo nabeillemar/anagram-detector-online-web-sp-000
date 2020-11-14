b93176f1cb8b36d0058927ea6a4fde8eab7d7558
Nabeil Lemar <nabeillemar@gmail.com>
2020-11-14 20:37:48 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,6 +11,7 @@ end
 def match(array_of_words) #takes an array of possible anagrams
   array_of_words.select do |element|  #select grabs specific values in the given array
     (@word.split("").sort) == (element.split("").sort) #@word is listen and it is being split then sorted then it is being compared to the match element splited and sorted 
+    #we don't have to return anything because the block statement select already does that automatically
 end
 
 
